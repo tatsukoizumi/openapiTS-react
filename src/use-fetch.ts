fe34a87a1c0d97e56de907d608c9d0430303c6cd
tatsunori_koizumi <koizumitattsuu@gmail.com>
2022-12-07 22:41:27 +0900

    add use-fetch

---
+++ src/use-fetch.ts
@@ -0,0 +1,83 @@
+import { ApiError } from "./../schema-util"
+import {
+  ApiPathParam,
+  ApiQueryParam,
+  ApiResponse,
+  ExactPathByHttpMethod,
+} from "../schema-util"
+import useSWR, { SWRConfiguration } from "swr"
+import { createApiClient, RequestResponse } from "../api-client"
+import { useCallback, useMemo } from "react"
+
+type GetPath = ExactPathByHttpMethod<"get">
+
+type UseFetchOption<Path extends GetPath> = {
+  path: Path
+  params?: {
+    paths?: ApiPathParam<Path, "get">
+    query?: ApiQueryParam<Path, "get">
+  }
+  shouldCancel?: boolean
+  onSuccess?: (data: ApiResponse<Path, "get">) => void
+  onError?: (error: ApiError<Path, "get">) => void
+} & Omit<SWRConfiguration, "onSuccess" | "onError"> // revalidateOnFocus などのSWRのオプションも渡せるようにしておく
+
+export const useFetch = <Path extends GetPath>(
+  options: UseFetchOption<Path>
+) => {
+  const { path, params, onSuccess, onError, shouldCancel, ...swrOptions } =
+    options
+
+  const api = createApiClient({ path, httpMethod: "get", params })
+
+  const {
+    data: swrData,
+    mutate: swrMutate,
+    isValidating,
+  } = useSWR<RequestResponse<Path, "get">>(
+    shouldCancel ? null : api.path(),
+    () => api.request(),
+    {
+      onSuccess: res => {
+        if (res.result === "success" && !!onSuccess) {
+          onSuccess(res.data)
+          return
+        }
+        if (res.result === "error" && !!onError) {
+          onError(res.error)
+          return
+        }
+      },
+      onError: e => {
+        // unexpected error をハンドリング
+        console.error(e.message)
+      },
+
+      ...swrOptions,
+    }
+  )
+
+  const data = useMemo(() => {
+    if (!swrData) return undefined
+    if (swrData.result !== "success") return undefined
+    return swrData.data
+  }, [swrData])
+
+  const error = useMemo(() => {
+    if (!swrData) return undefined
+    if (swrData.result !== "error") return undefined
+    return swrData.error
+  }, [swrData])
+
+  const mutate = useCallback(
+    (data?: ApiResponse<Path, "get">, shouldRevalidate?: boolean) => {
+      swrMutate(
+        data ? { result: "success", data } : undefined,
+        shouldRevalidate
+      )
+    },
+    [swrMutate]
+  )
+
+  return { data, error, mutate, isValidating }
+}
